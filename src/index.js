489170ba28add86900000aae2ea7c7d4419dcf9a
LaToya Williams <latoyawilliams@LaToyas-MacBook-Pro.local>
2018-02-23 14:15:10 -0800

    Initial scaffolding.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+import SearchBar from './components/search_bar'
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const API_KEY = 'AIzaSyBNObCp1rZ1JIfkop4ngEykve5CH1hYVEA';
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+// Create anew component. This compnen should produce
+//some HTML.
+
+const App = () => {
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
+}
+
+// Take this component's generated HTML and put it
+// on the page (in DOM)
+
+ReactDOM.render(<App />, document.querySelector('.container'));
